3994b4cb246def4f4defe50ce89dc394f51b4f5d
senyukmaksim29-lgtm <senyukmaksim29@gmail.com>
2026-09-25 00:49:20 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'planner-cache-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './LargeTile.scale-100.png'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
